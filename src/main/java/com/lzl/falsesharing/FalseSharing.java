9cf106e06f4baf7903796e544be91a5d22c18ddd
lizanle <lizanle@xkeshi.com>
2018-01-09 10:37:40 +0800

    伪共享问题

---
+++ src/main/java/com/lzl/falsesharing/FalseSharing.java
@@ -0,0 +1,61 @@
+package com.lzl.falsesharing;
+
+/**
+ * 伪共享 导致的性能问题 示例
+ *
+ * Created by Lizanle on 2018/1/9.
+ */
+public class FalseSharing  implements Runnable{
+    // 根据cpu核心数 N ，从 1 逐个增加到 N 进行测试
+    private static  final int NUM_THREADS = 3;
+    public static final Long ITERATIONS = 500L * 1000L * 1000L;
+    private static ViolationLong[] longs = new ViolationLong[NUM_THREADS];
+    static {
+        for (int i = 0; i < longs.length; i++) {
+            longs[i] = new ViolationLong();
+        }
+    }
+    private final int arrayIndex;
+    public FalseSharing(int arrayIndex) {
+        this.arrayIndex = arrayIndex;
+    }
+
+    private final static class ViolationLong {
+        public volatile long value  = 0L;
+        // 这里我比较疑惑的是，缓存行为64字节，而这个填充只填充了 48个字节。应该填充56个字节才对。
+        public long p1,p2,p3,p4,p5,p6;  // 注释 或者 不注释 决定了是否填充缓存行
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        System.out.println(Runtime.getRuntime().availableProcessors());
+        runTest();
+        System.out.println("duration " + (System.currentTimeMillis() - start));
+        // 线程数 加了缓存行填充  没加缓存行填充
+        // 1     2802           2802
+        // 2     2898           13905
+        // 3     3146           27663
+        // 4     4113           28458
+    }
+
+    public static void runTest() throws InterruptedException {
+        Thread[] threads = new Thread[NUM_THREADS];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(new FalseSharing(i));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+    }
+
+    @Override
+    public void run() {
+        long i = ITERATIONS + 1;
+        while (0 != --i){
+            longs[arrayIndex].value = i;
+        }
+    }
+}
