734064b5515e7c987236472707d5407c0d82275e
lizanle <lizanle@xkeshi.com>
2018-02-02 10:34:17 +0800

    括号判断问题 直接用java的数据结构很快的解决

--- src/main/java/com/lzl/algo/leetCode/ValidParentheses.java
+++ src/main/java/com/lzl/algo/leetCode/ValidParentheses.java
@@ -9,12 +9,13 @@ import org.junit.Test;
  */
 public class ValidParentheses {
     public boolean isValid(String s){
-        Stack stack = new Stack();
+        java.util.Stack<Character> stack = new java.util.Stack<Character>();
+        stack.push('#');
         String demo = "(){}[]";
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            if(!isPair(stack.pop(), c)){
-                stack.push(new Node(c));
+            if(!isPair(stack.peek(), c)){
+                stack.push(c);
             }else {
                 stack.pop();
             }
@@ -30,101 +31,13 @@ public class ValidParentheses {
         String pairA = "()";
         String pairB = "[]";
         String pairC = "{}";
-        if((pairA.indexOf(a) >= 0 && pairA.indexOf(b) >=0) || (pairB.indexOf(a) >= 0 && pairB.indexOf(b) >=0) || (pairC.indexOf(a) >= 0 && pairC.indexOf(b) >=0)){
+        if((pairA.indexOf(a) >= 0 && pairA.indexOf(b) >0) || (pairB.indexOf(a) >= 0 && pairB.indexOf(b) >0) || (pairC.indexOf(a) >= 0 && pairC.indexOf(b) >0)){
             return true;
         }else{
             return false;
         }
     }
 
-    class Stack{
-        private Node top;
-        private Node button;
-
-        public Stack() {
-
-        }
-
-        public Node getTop() {
-            return top;
-        }
-
-        public void setTop(Node top) {
-            this.top = top;
-        }
-
-        public Node getButton() {
-            return button;
-        }
-
-        public void setButton(Node button) {
-            this.button = button;
-        }
-
-        public char pop(){
-            if(top == null && button == null){
-                return '#';
-            }
-            char pop = top.currentVal;
-            top = top.getPre();
-            if (top != null && top.getPre() != null) {
-                    top.getPre().setNext(null);
-            }
-            return pop;
-        }
-
-        public void push(Node node){
-            if(top  ==  null && button == null){
-                top = node;
-                button = node;
-                top.setPre(button);
-                return ;
-            }
-            if(top != null) {
-                top.next = node;
-                node.setPre(top);
-                top = node;
-            }else {
-                top = node ;
-                button.next = top;
-                top.pre = button;
-            }
-        }
-    }
-
-    class Node{
-        private Node next;
-        private Node pre;
-        private char currentVal;
-
-        public Node(char currentVal) {
-            this.currentVal = currentVal;
-        }
-
-        public Node getNext() {
-            return next;
-        }
-
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
-        public char getCurrentVal() {
-            return currentVal;
-        }
-
-        public void setCurrentVal(char currentVal) {
-            this.currentVal = currentVal;
-        }
-
-        public Node getPre() {
-            return pre;
-        }
-
-        public void setPre(Node pre) {
-            this.pre = pre;
-        }
-    }
 
     @Test
     public void test(){
