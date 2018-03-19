35c3ecf6d8bbd154a002afc9def08417a6061d99
lizanle <491823689@qq.com>
2018-03-19 22:17:55 +0800

    BIO 时间服务器

---
+++ src/main/java/com/lzl/netty/chapter2/TimeServer.java
@@ -0,0 +1,38 @@
+package com.lzl.netty.chapter2;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Created by lizanle on 2018/3/19.
+ */
+public class TimeServer {
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if( args != null && args.length > 0){
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+
+            }
+        }
+
+        ServerSocket server = null;
+        try {
+             server = new ServerSocket(port);
+             System.out.println("time server is start in port:"+port);
+            Socket socket = null;
+            while (true) {
+                 socket = server.accept();
+                 new Thread(new TimeServerHandler(socket)).start();
+            }
+        } finally {
+            if(server != null){
+                System.out.println("time server close ");
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
