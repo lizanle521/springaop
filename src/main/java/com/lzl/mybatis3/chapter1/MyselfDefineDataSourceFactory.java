20b7a988e0687d83b5eb0f2bde9d29af9f6b5ae5
lizanle <491823689@qq.com>
2018-05-24 12:31:28 +0800

    mybatis 更换连接池

--- src/main/java/com/lzl/mybatis3/chapter1/MyselfDefineDataSourceFactory.java
+++ src/main/java/com/lzl/mybatis3/chapter1/MyselfDefineDataSourceFactory.java
@@ -3,9 +3,26 @@ package com.lzl.mybatis3.chapter1;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
 
-public class MyselfDefineDataSourceFactory extends UnpooledDataSourceFactory {
+import javax.sql.DataSource;
+import java.util.Properties;
 
+public class MyselfDefineDataSourceFactory extends UnpooledDataSourceFactory {
+    private Properties properties;
     public MyselfDefineDataSourceFactory() {
-        this.dataSource = new DruidDataSource();
+    }
+
+    @Override
+    public DataSource getDataSource() {
+        DruidDataSource dds = new DruidDataSource();
+        dds.setDriverClassName(this.properties.getProperty("driver"));
+        dds.setUrl(this.properties.getProperty("url"));
+        dds.setUsername(this.properties.getProperty("username"));
+        dds.setPassword(this.properties.getProperty("password"));
+        return dds;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        this.properties = properties;
     }
 }
