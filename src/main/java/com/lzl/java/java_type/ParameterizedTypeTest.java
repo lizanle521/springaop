7bf7b911ed7cecce2db8b5e239fca201d294094b
lizanle <491823689@qq.com>
2018-10-25 09:40:16 +0800

    java 的 泛型实质

--- src/main/java/com/lzl/java/java_type/ParameterizedTypeTest.java
+++ src/main/java/com/lzl/java/java_type/ParameterizedTypeTest.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * 在运行期间，如果方法中出现带泛型的匿名内部类，那么泛型依旧会被保留下来，我们可以通过对应的方法获取到实际的泛型类型
+ * @param <T>
+ */
 public class ParameterizedTypeTest<T> {
     private List<T> list = null;
     private Set<T> set = null;
@@ -14,12 +18,14 @@ public class ParameterizedTypeTest<T> {
     private Map.Entry<T,T> entry;
     private T[] ts = null;
     private List<String>[] lists = null;
+    private List<? extends Number> numbers = null;
+    private List<? super Number> nms = null;
 
     @Test
     public void test1() throws NoSuchFieldException {
         //ParameterizedType表示参数化类型，也就是泛型，例如List<T>、Set<T>等
 
-        Field list = ParameterizedTypeTest.class.getDeclaredField("t");
+        Field list = ParameterizedTypeTest.class.getDeclaredField("nms");
         // 获取属性的类型
         Type genericType = list.getGenericType();
 
@@ -34,6 +40,19 @@ public class ParameterizedTypeTest<T> {
             Type[] actualTypeArguments = type.getActualTypeArguments();
             for (Type actualTypeArgument : actualTypeArguments) {
                 System.out.println("actual type arg:" + actualTypeArgument);
+                // 如果是通配符类型，那么就可以找通配的上界（extens 和 super都有） 和 下界（extends没有，super有）
+                if(actualTypeArgument instanceof WildcardType){
+                    WildcardType wildcardType = (WildcardType) actualTypeArgument;
+                    Type[] lowerBounds = wildcardType.getLowerBounds();
+                    for (Type lowerBound : lowerBounds) {
+                        System.out.println("lowerBounds:"+lowerBound);
+                    }
+                    Type[] upperBounds = wildcardType.getUpperBounds();
+                    for (Type upperBound : upperBounds) {
+                        System.out.println("upperBounds:"+upperBound);
+                    }
+
+                }
             }
 
             // 获取泛型T的拥有者 null
@@ -55,6 +74,7 @@ public class ParameterizedTypeTest<T> {
             // 打印泛型名称
             System.out.println(typeVariable.getName());
             // 获取声明该类型变量实体 ,譬如T类型是 ParameterizedTypeTest声明的
+            // GenericDeclaration下有三个子类，分别为Class、Method、Constructor；也就是说，我们定义泛型只能在一个类中这3个地方自定义泛型；
             GenericDeclaration genericDeclaration = typeVariable.getGenericDeclaration();
 
             System.out.println(genericDeclaration);
