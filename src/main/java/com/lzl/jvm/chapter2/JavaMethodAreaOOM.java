312440023db1c81e5391092edb23a4d120df28e1
lizanle <491823689@qq.com>
2018-11-01 17:29:48 +0800

    方法区溢出

---
+++ src/main/java/com/lzl/jvm/chapter2/JavaMethodAreaOOM.java
@@ -0,0 +1,31 @@
+package com.lzl.jvm.chapter2;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * -XX:MaxMetaspaceSize=5m
+ */
+public class JavaMethodAreaOOM {
+    public static void main(String[] args) {
+        while (true) {
+            Enhancer enhancer = new Enhancer();
+            enhancer.setSuperclass(OOMObject.class);
+            enhancer.setUseCache(false);
+            enhancer.setCallback(new MethodInterceptor() {
+                @Override
+                public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+                    return methodProxy.invoke(o, objects);
+                }
+            });
+            enhancer.create();
+        }
+    }
+
+    static class OOMObject{
+
+    }
+}
