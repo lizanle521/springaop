db3119a27e23866bb5bf4ba4fdd81188abb22db7
lizanle <491823689@qq.com>
2018-11-01 17:08:59 +0800

    java8的常量池 在 堆内存里边

---
+++ src/main/java/com/lzl/jvm/chapter2/ConstantPoolOOM.java
@@ -0,0 +1,15 @@
+package com.lzl.jvm.chapter2;
+
+import java.util.ArrayList;
+
+/**
+ * -Xmx2m -Xms2m
+ */
+public class ConstantPoolOOM {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 1000000; i++) {
+            list.add(String.valueOf(i+"---------").intern());
+        }
+    }
+}
