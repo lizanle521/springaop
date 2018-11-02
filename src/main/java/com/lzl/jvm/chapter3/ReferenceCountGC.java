d977416cf9f86ac27457de8aaa69cc225782676c
lizanle <491823689@qq.com>
2018-11-02 16:26:05 +0800

    gc 回收问题。

---
+++ src/main/java/com/lzl/jvm/chapter3/ReferenceCountGC.java
@@ -0,0 +1,32 @@
+package com.lzl.jvm.chapter3;
+
+import org.junit.Test;
+
+/**
+ * -verbose:gc
+ */
+public class ReferenceCountGC {
+    public Object instance = null;
+    private static final int  _1mb = 1024*1024;
+    private byte[] bigSize = new byte[ 1 * _1mb];
+
+    @Test
+    public void testGC(){
+        ReferenceCountGC objA = new ReferenceCountGC();
+        ReferenceCountGC objB = new ReferenceCountGC();
+        objA.instance = objA;
+        objB.instance = objB;
+         objA = null;
+         objB = null;
+
+            //ReferenceCountGC a = null;
+           // ReferenceCountGC b = null;
+        System.out.println("free before:"+Runtime.getRuntime().freeMemory()/1024/1024);// 167  167
+
+        // 调用 gc，但是 objA 和 objB 会被回收
+         System.gc();
+         // 通过是否注释   objA = null;
+        //         objB = null; 这两行代码来确定，注释了以后，可用内存增加了 2
+        System.out.println("free after:"+Runtime.getRuntime().freeMemory()/1024/1024);// 175 177
+    }
+}
