55de49e608e39ad282bb0cc59a8033a98e68acf0
lizanle <491823689@qq.com>
2018-11-13 11:10:19 +0800

    内存对象布局分析

--- src/main/java/com/lzl/jvm/chapter3/TestTenuringThreshold.java
+++ src/main/java/com/lzl/jvm/chapter3/TestTenuringThreshold.java
@@ -8,11 +8,12 @@ public class TestTenuringThreshold {
 
     public static void main(String[] args) {
         byte[] all1,all2,all3,all4;
-        all1 = new byte[_1mb];
-        all2 = new byte[4*_1mb];
-        //all3 = new byte[4*_1mb];
+        all1 = new byte[_1mb/4]; // 第一次分配 0.25m 在eden区
+        all2 = new byte[4*_1mb]; // 第二次分配 4m 在eden区 ，此时已经占用了6m,接下来是没有空间再分配4m了
+        all3 = new byte[4*_1mb]; // 第三次分配4m 在eden区，发现内存不够，进行gc,由于对象 1,2还有引用，无法回收，那么就直接分配到老年代
 
-        //all3 = null;
-        //all3 = new byte[4*_1mb];
+        all3 = null; // 老年代对象引用置空
+        all3 = new byte[4*_1mb]; //再次分配 4m 再eden区，发现内存不够，进行一次gc,由于MaxTenuringThreshold的值为1，对象1,2被提升到老年代
+        // 最后内存布局为 eden区新对象 3，老年代对象为 1,2
     }
 }
