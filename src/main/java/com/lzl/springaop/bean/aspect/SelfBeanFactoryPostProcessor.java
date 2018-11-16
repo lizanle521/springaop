452754b37e55834a672082ae2d2163301dd4c945
lizanle <491823689@qq.com>
2018-11-16 17:47:59 +0800

    打印消息查看方法的调用顺序

---
+++ src/main/java/com/lzl/springaop/bean/aspect/SelfBeanFactoryPostProcessor.java
@@ -0,0 +1,12 @@
+package com.lzl.springaop.bean.aspect;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+
+public class SelfBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("SelfBeanFactoryPostProcessor.postProcessBeanFactory");
+    }
+}
