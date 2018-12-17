8e5016bff66f147be6b1ca83d6ed027bc6316462
lizanle <491823689@qq.com>
2018-12-17 16:21:24 +0800

    jmh 消耗cpu时间

---
+++ src/main/java/com/lzl/jmh/BenchMarkConsumeCpu.java
@@ -0,0 +1,77 @@
+package com.lzl.jmh;
+
+import org.junit.Test;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class BenchMarkConsumeCpu {
+    @Benchmark
+    public void consumer_000(){
+        Blackhole.consumeCPU(0);
+    }
+
+    @Benchmark
+    public void consumer_001(){
+        Blackhole.consumeCPU(1);
+    }
+
+    @Benchmark
+    public void consumer_002(){
+        Blackhole.consumeCPU(2);
+    }
+
+    @Benchmark
+    public void consume_0004() {
+        Blackhole.consumeCPU(4);
+    }
+
+    @Benchmark
+    public void consume_0008() {
+        Blackhole.consumeCPU(8);
+    }
+
+    @Benchmark
+    public void consume_0016() {
+        Blackhole.consumeCPU(16);
+    }
+
+    @Benchmark
+    public void consume_0128() {
+        Blackhole.consumeCPU(128);
+    }
+
+    @Benchmark
+    public void consume_0256() {
+        Blackhole.consumeCPU(256);
+    }
+
+    @Benchmark
+    public void consume_0512() {
+        Blackhole.consumeCPU(512);
+    }
+
+    @Benchmark
+    public void consume_1024() {
+        Blackhole.consumeCPU(1024);
+    }
+
+    @Test
+    public void test() throws RunnerException {
+        Options build = new OptionsBuilder()
+                .include(BenchMarkConsumeCpu.class.getSimpleName())
+                .forks(1)
+                .build();
+        new Runner(build).run();
+    }
+}
