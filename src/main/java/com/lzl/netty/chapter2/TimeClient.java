d584746f7c3c53833b1714bed9b0fc4aa1a6e58f
lizanle <491823689@qq.com>
2019-02-15 23:17:44 +0800

    出现阻塞问题

---
+++ src/main/java/com/lzl/netty/chapter2/TimeClient.java
@@ -0,0 +1,73 @@
+package com.lzl.netty.chapter2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @author lizanle
+ * @data 2019/2/15 8:10 PM
+ */
+public class TimeClient {
+
+    public static void main(String[] args) {
+        int port = 8081;
+        if(args != null && args.length > 0)
+        {
+            try {
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        Socket socket = null;
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        try {
+            socket = new Socket("127.0.0.1", port);
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            out = new PrintWriter(socket.getOutputStream(),true);
+
+            out.print("QUERY TIME ORDER");
+
+            System.out.println("Send order 2 server succeed.");
+
+            String s = in.readLine();
+
+            System.out.println("now is " + s);
+
+        }catch (Exception e){
+
+        }finally {
+            if(out != null){
+                out.close();
+                out = null;
+            }
+
+            if(in != null)
+            {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                in = null;
+            }
+
+            if(socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                socket = null;
+            }
+
+        }
+    }
+}
