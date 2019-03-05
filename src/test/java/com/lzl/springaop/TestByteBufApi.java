05c489379c87219bfda11f8017ce6f9b93e6961b
lizanle <491823689@qq.com>
2019-03-05 18:27:01 +0800

    bytebufapi

--- src/test/java/com/lzl/springaop/TestByteBufApi.java
+++ src/test/java/com/lzl/springaop/TestByteBufApi.java
@@ -2,6 +2,7 @@ package com.lzl.springaop;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.PooledByteBufAllocator;
+import io.netty.buffer.UnpooledByteBufAllocator;
 import org.junit.Test;
 
 /**
@@ -34,5 +35,77 @@ public class TestByteBufApi {
         byte b = byteBuf.readByte();
         assert  b == 1;
         assert byteBuf.readerIndex() == 1;
+    }
+
+    @Test
+    public void readwriteUnsignedByte(){
+        ByteBuf byteBuf;
+        byteBuf = UnpooledByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeShort(1);
+        assert  byteBuf.writerIndex() == 2;
+        assert byteBuf.readerIndex() == 0;
+        assert byteBuf.capacity() == 256;
+        int unsignedShort = byteBuf.readUnsignedShort();
+        assert unsignedShort == 1;
+        assert byteBuf.readerIndex() == 2;
+    }
+
+    @Test
+    public void readwriteMedium(){
+        ByteBuf byteBuf;
+        byteBuf = UnpooledByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeMedium(1);
+
+        assert byteBuf.writerIndex() == 3;
+        assert byteBuf.readerIndex() == 0;
+        assert byteBuf.capacity() == 256;
+        int i = byteBuf.readMedium();
+        assert byteBuf.readerIndex() == 3;
+        assert byteBuf.readableBytes() == 0;
+        assert i == 1;
+    }
+
+    @Test
+    public void readwriteInt(){
+        ByteBuf byteBuf;
+        byteBuf = UnpooledByteBufAllocator.DEFAULT.buffer();
+        byteBuf.writeInt(1);
+        assert byteBuf.writerIndex() == 4;
+        assert byteBuf.readerIndex() == 0;
+        assert byteBuf.capacity() == 256;
+        int i = byteBuf.readInt();
+        assert i == 1;
+        assert byteBuf.readerIndex() == 4;
+    }
+
+    @Test
+    public void readwriteBytes(){
+        ByteBuf byteBuf;
+        byteBuf = UnpooledByteBufAllocator.DEFAULT.buffer();
+
+        ByteBuf bf;
+        bf = UnpooledByteBufAllocator.DEFAULT.buffer();
+        bf.writeInt(1);
+        ///ByteBuf.writeBytes(ByteBuf bf) 将源bf中所有刻度字节写入到当前bytebuffer.操作成功以后，当前bytebuf的writeindex = writerindex + bf.readablebytes;
+        byteBuf.writeBytes(bf);
+
+        assert byteBuf.writerIndex() == 4;
+        assert byteBuf.readerIndex() == 0;
+        assert bf.readerIndex() == 4;
+
+        int i = byteBuf.readInt();
+        assert  i == 1;
+        assert  byteBuf.readerIndex() == 4;
+
+
+        //ByteBuf.writeBytes(ByteBuf src,int length) 将源ByteBuf中的可读字节写入到当前bytebuf中，写入的字节长度数为length。
+        // 操作成功以后，当前bytebuf 的writeIndex = writeIndex + length;
+        bf.writeLong(8L);
+        assert bf.writerIndex() == 12;
+        byteBuf.writeBytes(bf,8);
+        assert byteBuf.writerIndex() == 12;
+        long l = byteBuf.readLong();
+        assert l == 8L;
+
     }
 }
