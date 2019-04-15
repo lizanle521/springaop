b6b878393e8de8dcf1e8ba15cf06d65ed5df80e4
lizanle <491823689@qq.com>
2019-04-15 14:57:22 +0800

    乱码解决方案

--- src/main/java/com/lzl/design_pattern/S_State/Main.java
+++ src/main/java/com/lzl/design_pattern/S_State/Main.java
@@ -1,11 +1,13 @@
 package com.lzl.design_pattern.S_State;
 
 /**
+ * 这里可能会乱码，需要加虚拟机参数 -Dfile.encoding=gbk
  * @author lizanle
  * @Date 2019/4/15 14:37
  */
 public class Main {
     public static void main(String[] args) {
+        //System.setProperty("file.encoding","gbk");
         new SafeFrame("状态模式举例");
     }
 }
